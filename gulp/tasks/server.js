8027679a6a26a845116b56e3f85a1f91f7ec5780
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2022-01-16 02:15:25 +0200

    Add server task

---
+++ gulp/tasks/server.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+export default function server() {
+  app.plugins.browsersync.init({
+    server: {
+      baseDir: app.path.build.html,
+    },
+    notify: false,
+    port: 3000,
+  });
+}
