18ccc10df92d4ad64037ee1b2d382d935e252bba
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2022-01-16 02:15:01 +0200

    Add html task

---
+++ gulp/tasks/html.js
@@ -0,0 +1,34 @@
+import fileInclude from 'gulp-file-include';
+import webpHtmlNoSvg from 'gulp-webp-html-nosvg';
+import versionNumber from 'gulp-version-number';
+
+/* eslint-disable no-undef */
+export default function html() {
+  return app.gulp.src(app.path.src.html)
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: 'HTML',
+        message: 'Error: <%= error.message %>',
+      }),
+    ))
+    .pipe(fileInclude())
+    .pipe(webpHtmlNoSvg())
+    .pipe(
+      versionNumber({
+        value: '%DT%',
+        append: {
+          key: '_v',
+          cover: 0,
+          to: [
+            'css',
+            'js',
+          ],
+        },
+        output: {
+          file: 'gulp/config/version.json',
+        },
+      }),
+    )
+    .pipe(app.gulp.dest(app.path.build.html))
+    .pipe(app.plugins.browsersync.stream());
+}
